d84391359451f5f0488003941d44a2ef4464c12a
reds98 <sahid.rojas64@gmail.com>
2024-01-22 16:08:46 -0600

    Updated the ticket generated layout and added other information

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/generate-pdf', async (req, res) => {
-  const { nombre, fecha,hora } = req.body;
+  const { nombre, fecha,hora,campos } = req.body;
 
   try {
-    const existingPdfBytes = fs.readFileSync('entradaSENTI34.pdf');
+    const existingPdfBytes = fs.readFileSync('EntradaSentipensante.pdf');
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
     const page = pdfDoc.getPage(0);
 
     page.drawText(fecha, { x: 43, y: 42, size: 10, color: rgb(0, 0, 0) });
-    page.drawText(hora, { x: 140, y: 42, size: 10, color: rgb(0, 0, 0) });
-    page.drawText(nombre, { x: 80, y: 23, size: 10, color: rgb(0, 0, 0) });
+    page.drawText(hora, { x: 150, y: 42, size: 10, color: rgb(0, 0, 0) });
+    page.drawText(nombre, { x: 43, y: 20, size: 10, color: rgb(0, 0, 0) });
+    page.drawText(campos, { x: 150, y: 20, size: 10, color: rgb(0, 0, 0) });
    
 
     const pdfBytes = await pdfDoc.save();
